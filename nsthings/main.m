b2ace413eceb4265ef57b74452ed9be9215e0d8e
F839-014 M <mf839-014@70-12-242-27.pools.spcsdns.net>
2016-06-17 14:00:33 +0900

    Initial Commit

---
+++ nsthings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nsthings
+//
+//  Created by MF839-014 on 2016. 6. 17..
+//  Copyright © 2016년 MF839-014. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
